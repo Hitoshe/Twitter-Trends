6ad7a5126503af40af442514d251202a2a277142
unknown <martsinovichivan@gmail.com>
2025-04-15 10:01:34 +0300

    Изменена азритектура проекта на Core и UI

--- Twitter-Trends.Core/Program.cs
+++ Twitter-Trends.Core/Program.cs
@@ -1,14 +1,10 @@
-﻿using NetTopologySuite.Geometries;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
-using SFML.Graphics;
-using SFML.Window;
-using SFML.System;
 
 namespace Twitter_Trends
 {
@@ -18,9 +14,9 @@ namespace Twitter_Trends
         public Dictionary<string, double> sentiments;
         public List<Tweet> tweets;
 
-        private static string? tweetsPath;          //  Directory
-        private static string? sentimentsPath;
-        private static string? statesPath;
+        private static string tweetsPath;          //  Directory
+        private static string sentimentsPath;
+        private static string statesPath;
 
         static void Main(string[] args)
         {
